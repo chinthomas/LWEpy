587329b0a3e11f548d1169a90f26de737c21ff5e
chinthomas <thomas.chin.2002@gmail.com>
2023-12-25 09:16:49 +0800

    fix: decode problem

--- PLWE.py
+++ PLWE.py
@@ -1,7 +1,7 @@
 from numpy.polynomial import Polynomial
 from numpy.random import*
 import numpy as np
-class PLWE:
+class RLWE:
     def __init__(self, n, q) -> None:
         self.n = n
         self.q = q
@@ -13,6 +13,7 @@ class PLWE:
         A = A.round(0).astype(int)
         polyA = Polynomial(A)
         return polyA
+    
     @staticmethod
     def errorPoly(n:int) -> Polynomial:
         e: np.ndarray = rand(n).round(0).astype(int)
@@ -44,38 +45,44 @@ class PLWE:
 
     def keyGen(self):
         A = self.randomPoly(self.n, self.q)
-        self.S = self.randomPoly(self.n, self.q)
+        self._S = self.randomPoly(self.n, self.q)
         e = self.errorPoly(self.n)
-        T = A * self.S + e
+        T = A * self._S + e
         T = self.mod(T)
-        return A, T
+        return (A, T)
 
-    def enc(self, A:Polynomial, T:Polynomial, m):
+    def enc(self, pk:(Polynomial,Polynomial), m:list):
+        M = Polynomial(m) * (self.q//2)
+        A, T = pk
         r = self.randomPoly(self.n, self.q)
         e1 = self.errorPoly(self.n)
         e2 = self.errorPoly(self.n)
         U = A * r + e1
         U = self.mod(U)
-        C = T * r + e2 + self.q//2*m
+        C = T * r + e2 + M
         C = self.mod(C)
-        return U, C
+        return (U, C)
 
-    def dec(self, U:Polynomial, C:Polynomial):
+    def dec(self, cipher:(Polynomial,Polynomial)):
         """
             ** how to rounding decoding message ???
         """
-        M = C - U * self.S
+        U, C = cipher
+        M = C - U * self._S
         M = self.mod(M)
-        print(M)
-        return M
+        m = M.coef//(self.q//2)
+        return m.astype(int)
 
 if __name__ == "__main__":
     n=7
     q=13
+    m1 = np.array([1,0,0,1,0,0,0])
     seed(0)
-    PLWE_test = PLWE(n,q)
-    A, T = PLWE_test.keyGen()
-    U, C = PLWE_test.enc(A, T, 1)
-    M = PLWE_test.dec(U,C)
+    PLWE_test = RLWE(n,q)
+    pk = PLWE_test.keyGen()
+    cipher = PLWE_test.enc(pk, m1)
+    m2 = PLWE_test.dec(cipher)
+    print("m1:", m1)
+    print("m2:", m2)
     # PLWE_test.polyToMatrix(A)
     # PLWE_test.polyToMatrix(T)
---
+++ polynomail.py
@@ -0,0 +1,4 @@
+from numpy.polynomial import Polynomial
+
+p = Polynomial([1,2,3])
+print (p)
